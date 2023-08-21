82bd56ec8d84322b69ee8801059c1dcb507cf1eb
Tano Paul <tanopaul38@gmail.com>
2023-08-21 13:38:13 -0400

    completed hooks import export

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import Home from './Home.js';
+import About from './About.js';
+import NavBar from "./NavBar.js";
+import {username, image, city} from '../data/user.js';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+        name = {username}
+        city = {city}
+      />
+      <About 
+        image = {image}
+      />
     </div>
   );
 }
